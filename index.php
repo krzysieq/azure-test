e322bfde817819aceb7c4affac7856497850b708
Krzysiek Wróblewski <krzysiek.wr@gmail.com>
2014-10-21 00:38:20 +0100

    move the database connection code to a separate file

--- index.php
+++ index.php
@@ -13,21 +13,8 @@
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
-    // DB connection info
-    //TODO: Update the values for $host, $user, $pwd, and $db
-    //using the values you retrieved earlier from the portal.
-    $host = "eu-cdbr-azure-west-b.cloudapp.net";
-    $user = "b0fad7066acd5d";
-    $pwd = "5861d6a2";
-    $db = "comp2013AoRruCPu";
-    // Connect to database.
-    try {
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch(Exception $e){
-        die(var_dump($e));
-    }
+    require_once("db.php");
+    
     // Insert registration info
     if(!empty($_POST)) {
     try {
